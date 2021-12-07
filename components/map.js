99421c03bb8e000d6acebfb713180444d151159b
MrudulaNNaik <mrudulannaik1999@gmail.com>
2021-12-07 11:13:56 +0530

    Map App commit

--- components/map.js
+++ components/map.js
@@ -1,62 +1,296 @@
- import React, {useState, Component} from 'react';
- import {StyleSheet, View} from 'react-native';
- import MapView from 'react-native-maps';
- import {Marker, Polyline} from 'react-native-maps';
- //import MapViewDirections from 'react-native-maps-directions';
- 
- const map = () => {
-   const [coordinates] = useState([
-     {
-       latitude: 48.8587741,
-       longitude: 2.2069771,
-     },
-     {
-       latitude: 48.8323785,
-       longitude: 2.3361663,
-     },
-   ]);
-   return (
-     <View style={styles.MainContainer}>
-       <MapView
-         style={styles.mapStyle}
-        
-showsUserLocation={true}    zoomEnabled={true}  zoomControlEnabled={true}
+import React from 'react';
+import {View, Text, PermissionsAndroid, Alert, Platform} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import MapView, {PROVIDER_GOOGLE, Marker, Polyline} from 'react-native-maps';
 
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      coordinates: [],
+    };
+  }
 
-         initialRegion={{
-           latitude: coordinates[0].latitude,
-           longitude: coordinates[0].longitude,
-           latitudeDelta: 0.0622,
-           longitudeDelta: 0.0121,
-         }}>
-         <Marker coordinate={coordinates[0]} />
-         <Marker coordinate={coordinates[1]} />
-         <Polyline
-           coordinates={coordinates}
-           strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
-           strokeColors={['#7F0000']}
-           strokeWidth={6}
-         />
-       </MapView>
-     </View>
-   );
- };
- export default map ;
- const styles = StyleSheet.create({
-   MainContainer: {
-     position: 'absolute',
-     top: 0,
-     left: 0,
-     right: 0,
-     bottom: 0,
-     alignItems: 'center',
-     justifyContent: 'flex-end',
-   },
-   mapStyle: {
-     position: 'absolute',
-     top: 0,
-     left: 0,
-     right: 0,
-     bottom: 0,
-   },
- });
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordinates: this.state.coordinates.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
+      },
+      error => {
+        Alert.alert(error.message.toString());
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+      },
+    );
+
+    Geolocation.watchPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordinates: this.state.coordinates.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
+      },
+      error => {
+        console.log(error);
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+        distanceFilter: 0,
+      },
+    );
+  }
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={mapStyle}
+          style={{flex: 1}}
+          
+          zoomEnabled={true}
+          zoomControlEnabled={true}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: this.state.latitude,
+              longitude: this.state.longitude,
+            }}></Marker>
+            <Polyline
+            coordinates={this.state.coordinates}
+            strokeColor="red"
+            strokeColors={[
+              '#bf8221',
+              '#ffe066',
+              '#ffe066',
+              '#ffe066',
+              '#ffe066',
+            ]}
+            strokeWidth={3}
+          />
+          
+        </MapView>
+      </View>
+    );
+  }
+}
+
+export const mapStyle = [
+  
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#212121',
+      },
+    ],
+  },
+  {
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#212121',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.country',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#9e9e9e',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#bdbdbd',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#181818',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#1b1b1b',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#2c2c2c',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#8a8a8a',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#373737',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#3c3c3c',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#4e4e4e',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#000000',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#3d3d3d',
+      },
+    ],
+  },
+];
